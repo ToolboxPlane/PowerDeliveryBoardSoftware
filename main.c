91c48b81a8c36c5dc152fe3ee2960594ae822818
Paul Nykiel <paul.nykiel@gmail.com>
2019-04-24 22:07:28 +0200

    Added the main code

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
 #include "Drivers/ltc2946.h"
+#include "Util/communication.h"
 
 #define LTC_VCC_ADDR 0xDE
 #define LTC_5V_ADDR 0xCE
@@ -11,13 +12,24 @@ int main() {
     cli();
     ltc2946_init(LTC_VCC_ADDR,  0, 0xFFFFFFFF, 14000, 20000, 0, 1000);
     ltc2946_init(LTC_5V_ADDR,  0, 0xFFFFFFFF, 4500, 5500, 0, 500);
+    communication_init();
     // Enable the interrupts
     sei();
     wdt_enable(WDTO_120MS);
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
+    uint8_t status_5V, status_vcc;
+    ltc_result_t meas_5V, meas_vcc;
     while (true) {
+        status_vcc = ltc2946_status(LTC_VCC_ADDR);
+        status_5V = ltc2946_status(LTC_5V_ADDR);
+        ltc2946_read(LTC_VCC_ADDR, &meas_vcc);
+        ltc2946_read(LTC_5V_ADDR, &meas_5V);
+
+        communication_send_data(status_5V | status_vcc,
+                &meas_vcc, &meas_5V);
+
         wdt_reset();
     }
 #pragma clang diagnostic pop
